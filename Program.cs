2af17b317d578f1670076d0e6880f88c228093bc
AlexanderVasilevv <119069243+AlexanderVasilevv@users.noreply.github.com>
2022-11-25 22:04:00 +0200

    Add files via upload
    mini max sum

--- Program.cs
+++ Program.cs
@@ -1,54 +1,13 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
-class Solution
+namespace mini_max_sum
 {
-
-    
-    static void plusMinus(int[] arr)
+    class Program
     {
-        double pos = 0;
-        double neg = 0;
-        double zero = 0;
-
-        foreach (int num in arr)
+        static void Main(string[] args)
         {
-            if (num > 0)
-            {
-                pos++;
-            }
-            else if (num < 0)
-            {
-                neg++;
-            }
-            else
-            {
-                zero++;
-            }
+            Console.WriteLine("Hello World!");
         }
-
-        Console.WriteLine(pos / arr.Length);
-        Console.WriteLine(neg / arr.Length);
-        Console.WriteLine(zero / arr.Length);
-    }
-
-    static void Main(string[] args)
-    {
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
-        ;
-        plusMinus(arr);
     }
 }
+
